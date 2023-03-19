195fe1d6013b5c867153c1d884d907c7962a74ea
dxxvii <89101651+dxxvii@users.noreply.github.com>
2023-03-19 18:06:11 +0100

    Delete quijote directory

--- quijote/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-  const header = document.querySelector('header');
-  const distanciaY = window.pageYOffset || document.documentElement.scrollTop;
-  if (distanciaY > 100) {
-    header.classList.add('mostrar');
-  } else {
-    header.classList.remove('mostrar');
-  }
-});
